9bf38cb1518609510c522bb41c9c49ad5b8d5bdb
Fede-84-1 <cioeta.federico@gmail.com>
2026-04-23 16:28:21 +0200

    Update codiceArduinoAnno2024-2025.ino

--- codiceArduinoAnno2024-2025.ino
+++ codiceArduinoAnno2024-2025.ino
@@ -32,9 +32,9 @@ bool daly_rotate = false;
 #define ON_RIGHT_WALL_DISTANCE 0.125
 #define KP 0.37
 #define KD 4
-#define RIGHT_BASE_SPEED 185 //vista da davanti
+#define RIGHT_BASE_SPEED 210//185 //vista da davanti
 #define LEFT_BASE_SPEED (RIGHT_BASE_SPEED - 40)//vista da davanti
-#define ROTATION_SPEED 150
+#define ROTATION_SPEED 170//150
 #define MAX_TURN_SPEED 150
 #define FRONT_CM_DISTANCE_OBSTACLE_AVOIDANCE_THRESHOLD 15
 #define ALARM_COUNTER_THRESHOLD 10
